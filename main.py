57809b96e878b989aff888d514aab6e904414a2b
Пользователь <balakireva_maria@bk.ru>
2020-09-13 22:42:35 +0500

    add new function

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from urllib.parse import urlencode
 from pip._vendor import requests
+urrl = 'https://vk.com'
+url = 'https://oauth.vk.com/authorize'
+id = 7586347
+oauth_params = {'client_id': id, 'display':'page', 'scope':'status', 'response_type': 'token', 'v': 5.122 }
+print('?'.join((url, urlencode(oauth_params))))
+
 friend_list = []
+
 class Users:
     def __init__(self, token) -> None:
         self.token = token
@@ -12,10 +19,19 @@ class Users:
                           'v': 5.122})
         friends = self.r.json()['response']['items']
         return set(friends)
-    
 
-    
+    def __and__(self, other_user):
+        return print(self.get_friends() & other_user.get_friends())
 
-user1 = Users('b6224fa094f54ef3b6242b08db3e9414c5c7017538af0c0096da06758f8659da92cd633b52b4c68ace029')
+    def get_id(self):
+        self.r = requests.get('https://api.vk.com/method/users.get',
+                         params=
+                         {'access_token': self.token,
+                          'v': 5.122})
+        return print('/'.join((urrl,str(self.r.json()['response'][0]['id']))))
+
+
+user1 = Users('42decf32f1faf52dd4ca866282320e4f5f6bffce5f54f63f6ab36cd0e93dc143d95ffd6a5a0313e544f04')
 user2 = Users('10b2e6b1a90a01875cfaa0d2dd307b7a73a15ceb1acf0c0f2a9e9c586f3b597815652e5c28ed8a1baf13c')
-print(user1.get_friends() & user2.get_friends())
+user1 & user2
+user1.get_id()
